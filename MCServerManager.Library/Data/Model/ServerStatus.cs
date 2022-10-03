03f745684575cb2f0e862a3487cd63f52209f7de
yri066 <yri066.ru@gmail.com>
2022-10-03 23:20:59 +0300

    Состояния сервера

---
+++ MCServerManager.Library/Data/Model/ServerStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace MCServerManager.Library.Data.Model
+{
+	/// <summary>
+	/// Состояние сервера
+	/// </summary>
+	public class ServerStatus
+	{
+		public enum Status
+		{
+			Launch,
+			Run,
+			Stopping,
+			Off,
+			Error
+		}
+	}
+}
