d509278c339851bb4dcd66e60e09ab4df26db3f2
yri066 <yri066.ru@gmail.com>
2022-10-05 21:49:11 +0300

    Добавил страницу списка серверов

---
+++ MCServerManager/Pages/Server/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MCServerManager.Pages.Server
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
