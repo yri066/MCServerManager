a4dff3c0ae1a605922eb35642c763bc806de2912
yri066 <yri066.ru@gmail.com>
2022-10-05 19:37:43 +0300

    Добавил фоновую задачу для инициализации ServerService при запуске приложения.

---
+++ MCServerManager/Service/HostedService/RunGameServersHostedService.cs
@@ -0,0 +1,17 @@
+﻿using MCServerManager.Service;
+
+namespace MCServerManager.Service.HostedService
+{
+	public class RunGameServersHostedService : BackgroundService
+	{
+		public RunGameServersHostedService(ServerService serverService)
+		{
+
+		}
+
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
