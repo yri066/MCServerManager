ee9fc0b65f832e01fb10aab4a973881cab507ff9
yri066 <yri066.ru@gmail.com>
2022-10-11 11:26:07 +0300

    Страница списка серверов

--- MCServerManager/Pages/Server/List.cshtml.cs
+++ MCServerManager/Pages/Server/List.cshtml.cs
@@ -1,3 +1,4 @@
+using MCServerManager.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,13 @@ namespace MCServerManager.Pages.Server
 {
     public class ListModel : PageModel
     {
+        public readonly ServerService _service;
+
+        public ListModel(ServerService service)
+        {
+            _service = service;
+        }
+
         public void OnGet()
         {
         }
