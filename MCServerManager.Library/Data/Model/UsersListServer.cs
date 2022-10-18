b546f1700f1558a880682807304a5ebd60233485
yri066 <yri066.ru@gmail.com>
2022-10-18 20:17:19 +0300

    Класс для работы со списком игроков.

---
+++ MCServerManager.Library/Data/Model/UsersListServer.cs
@@ -0,0 +1,22 @@
+﻿namespace MCServerManager.Library.Data.Model
+{
+    public class UsersListServer
+    {
+        public Guid Version { get; private set; } = Guid.NewGuid();
+        public List<string> UserList { get; private set; } = new();
+
+        public int Count { get { return UserList.Count; } }
+
+        public void Add(string text)
+        {
+            UserList.Add(text);
+            Version = Guid.NewGuid();
+        }
+
+        public void Remove(string text)
+        {
+            UserList.Remove(text);
+            Version = Guid.NewGuid();
+        }
+    }
+}
