922fec42c63d96bb7a66e5dcf74bd2088345175a
yri066 <yri066.ru@gmail.com>
2022-10-18 20:22:21 +0300

    Класс для работы со списком игроков.

--- MCServerManager.Library/Data/Model/UsersListServer.cs
+++ MCServerManager.Library/Data/Model/UsersListServer.cs
@@ -1,22 +1,43 @@
 ﻿namespace MCServerManager.Library.Data.Model
 {
-    public class UsersListServer
-    {
-        public Guid Version { get; private set; } = Guid.NewGuid();
-        public List<string> UserList { get; private set; } = new();
+	/// <summary>
+	/// Список игроков
+	/// </summary>
+	public class UsersListServer
+	{
+		/// <summary>
+		/// Версия списка пользователей
+		/// </summary>
+		public Guid Version { get; private set; } = Guid.NewGuid();
 
-        public int Count { get { return UserList.Count; } }
+		/// <summary>
+		/// Список пользователей
+		/// </summary>
+		public List<string> UserList { get; private set; } = new();
 
-        public void Add(string text)
-        {
-            UserList.Add(text);
-            Version = Guid.NewGuid();
-        }
+		/// <summary>
+		/// Количество пользователей
+		/// </summary>
+		public int Count { get { return UserList.Count; } }
 
-        public void Remove(string text)
-        {
-            UserList.Remove(text);
-            Version = Guid.NewGuid();
-        }
-    }
+		/// <summary>
+		/// Добавление пользователя в список
+		/// </summary>
+		/// <param name="text">Имя пользователя</param>
+		public void Add(string text)
+		{
+			UserList.Add(text);
+			Version = Guid.NewGuid();
+		}
+
+		/// <summary>
+		/// Удаление пользователя из списка
+		/// </summary>
+		/// <param name="text">Имя пользователя</param>
+		public void Remove(string text)
+		{
+			UserList.Remove(text);
+			Version = Guid.NewGuid();
+		}
+	}
 }
