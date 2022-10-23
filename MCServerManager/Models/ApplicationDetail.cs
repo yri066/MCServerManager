7a1d3732bab6c6138a641d57836bb24b35456f5d
yri066 <yri066.ru@gmail.com>
2022-10-23 12:41:45 +0300

    Базовая модель приложения

---
+++ MCServerManager/Models/ApplicationDetail.cs
@@ -0,0 +1,42 @@
+﻿using MCServerManager.Data.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MCServerManager.Models
+{
+	/// <summary>
+	/// Данные о приложении
+	/// </summary>
+	public class ApplicationDetail
+	{
+		/// <summary>
+		/// Название приложения
+		/// </summary>
+		[Required, StringLength(100), DisplayName("Название сервера")]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Автозапуск
+		/// </summary>
+		[Required, DisplayName("Автозапуск")]
+		public bool AutoStart { get; set; }
+
+		/// <summary>
+		/// Расположение приложения
+		/// </summary>
+		[Required, DirectoryExists, DisplayName("Директория расположения сервера")]
+		public string WorkDirectory { get; set; }
+
+		/// <summary>
+		/// Программа для запуска
+		/// </summary>
+		[Required, StringLength(100), DisplayName("Программа для запуска")]
+		public string Programm { get; set; }
+
+		/// <summary>
+		/// Аргументы запуска
+		/// </summary>
+		[StringLength(100), DisplayName("Аргументы для запуска")]
+		public string? Arguments { get; set; }
+	}
+}
