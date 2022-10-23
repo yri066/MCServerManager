7f1b7a70441181cc9cea360ceca6a62de37dfd14
yri066 <yri066.ru@gmail.com>
2022-10-23 12:37:01 +0300

    Переименованы классы

---
+++ MCServerManager.Library/Data/Model/GameServerStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace MCServerManager.Library.Data.Model
+{
+	/// <summary>
+	/// Состояние сервера
+	/// </summary>
+	public class GameServerStatus
+	{
+		public enum Status
+		{
+			Launch,
+			Run,
+			Shutdown,
+			Off,
+			Reboot,
+			Error
+		}
+	}
+}
