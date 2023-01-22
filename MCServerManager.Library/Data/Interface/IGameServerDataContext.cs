a1cae5fd55c60b4a18d322591f0b106e35cd3f31
yri066 <yri066.ru@gmail.com>
2023-01-23 01:32:53 +0300

    Методы для работы с данными.

---
+++ MCServerManager.Library/Data/Interface/IGameServerDataContext.cs
@@ -0,0 +1,54 @@
+﻿using MCServerManager.Library.Data.Models;
+
+namespace MCServerManager.Library.Data.Interface
+{
+	public interface IGameServerDataContext
+	{
+		/// <summary>
+		/// Загружает информацию о серверах.
+		/// </summary>
+		/// <returns>Список данных о серверах.</returns>
+		public Task<List<Server>> LoadServerDataAsycn();
+
+		/// <summary>
+		/// Сохраняет информацию о серверах.
+		/// </summary>
+		public Task SaveServerDataAsycn(List<Server> gameServers);
+
+		/// <summary>
+		/// Добавить новый сервер.
+		/// </summary>
+		/// <param name="serverData">Информация о сервере.</param>
+		public Task CreateServerAsycn(Server gameServerData);
+
+		/// <summary>
+		/// Добавить новый сервис.
+		/// </summary>
+		/// <param name="serverData">Информация о сервисе.</param>
+		public Task CreateServiceAsycn(Service serviceData);
+
+		/// <summary>
+		/// Обновить информацию о сервере.
+		/// </summary>
+		/// <param name="serverData">Информация о сервере.</param>
+		public Task UpdateServerAsycn(Server serverData);
+
+		/// <summary>
+		/// Обновить информацию о сервисе.
+		/// </summary>
+		/// <param name="serverData">Информация о сервисе.</param>
+		public Task UpdateServiceAsycn(Service serviceData);
+
+		/// <summary>
+		/// Удалить сервер.
+		/// </summary>
+		/// <param name="id">Идентификатор сервера.</param>
+		public Task DeleteServerAsycn(Guid id);
+
+		/// <summary>
+		/// Удалить сервис.
+		/// </summary>
+		/// <param name="id">Идентификатор сервис.</param>
+		public Task DeleteServiceAsycn(Guid id);
+	}
+}
