ad3dbbc3cf1b8f59c6cea15aa1642f5fedd65e97
yri066 <yri066.ru@gmail.com>
2023-01-23 01:28:19 +0300

    Интерфейс для информация о серверном приложении.

---
+++ MCServerManager.Library/Data/Interface/IApplication.cs
@@ -0,0 +1,35 @@
+﻿namespace MCServerManager.Library.Data.Interface
+{
+	public interface IApplication
+	{
+		/// <summary>
+		/// Идентификатор приложения
+		/// </summary>
+		public Guid Id { get; }
+
+		/// <summary>
+		/// Название приложения
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// Расположение приложения
+		/// </summary>
+		public string WorkDirectory { get; }
+
+		/// <summary>
+		/// Программа для запуска
+		/// </summary>
+		public string StartProgram { get; }
+
+		/// <summary>
+		/// Аргументы запуска
+		/// </summary>
+		public string Arguments { get; }
+
+        /// <summary>
+        /// Название приложения
+        /// </summary>
+        public bool AutoStart { get; }
+    }
+}
