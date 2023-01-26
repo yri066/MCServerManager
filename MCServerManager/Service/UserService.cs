ccf95e9088189a180e51e589bde31d72f9f445b5
yri066 <yri066.ru@gmail.com>
2023-01-26 20:31:33 +0300

    Информация о зарегистрированном пользователе.

---
+++ MCServerManager/Service/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace MCServerManager.Service
+{
+    /// <summary>
+    /// Информация о зарегистрированном пользователе.
+    /// </summary>
+    public class UserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public string? UserId
+        {
+            get
+            {
+                return _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            }
+        }
+
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string? UserName
+        {
+            get
+            {
+                return _httpContextAccessor.HttpContext?.User.Identity?.Name;
+            }
+        }
+    }
+}
