f9175161d154c6d1dd6a539ce0d4c4720af42a9f
yri066 <yri066.ru@gmail.com>
2023-01-26 20:52:18 +0300

    Атрибуты проверки доступа у пользователя.

---
+++ MCServerManager/Data/FilterAttributes/UserServerAccessFilter.cs
@@ -0,0 +1,52 @@
+﻿using MCServerManager.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace MCServerManager.Data.FilterAttributes
+{
+    /// <summary>
+    /// Проверка доступа пользователя к серверу.
+    /// </summary>
+    public class UserServerAccessFilter : Attribute, IActionFilter
+    {
+        private readonly GameServerService _serverService;
+
+        public string Key = "id";
+        public string ErrorMessage = "Доступ запрещен.";
+
+        public UserServerAccessFilter(GameServerService serverService)
+        {
+            _serverService = serverService;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                context.ActionArguments.TryGetValue(Key, out object? value);
+                var userId = context.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var id = _serverService.GetServerData((Guid)value!).UserId;
+
+                if (id != userId)
+                {
+                    context.Result = new JsonResult(value: new { errorText = ErrorMessage }) { StatusCode = 403 };
+                }
+            }
+            catch (Exception)
+            {
+                context.Result = new JsonResult(value: new { errorText = ErrorMessage }) { StatusCode = 403 };
+            }
+
+            return;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
