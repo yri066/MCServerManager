c559271d8d12f0e84d44fa8f016f2891bc6b715e
yri066 <yri066.ru@gmail.com>
2023-10-30 00:41:00 +0300

    Добавлена пагинация.

--- MCServerManager/Pages/Server/List.cshtml.cs
+++ MCServerManager/Pages/Server/List.cshtml.cs
@@ -5,21 +5,73 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MCServerManager.Pages.Server
 {
-	public class ListModel : PageModel
-	{
-		private readonly GameServerService _service;
-		public readonly List<GameServer> Servers;
+    public class ListModel : PageModel
+    {
+        /// <summary>
+        /// Количество записей на странице по умолчанию.
+        /// </summary>
+        public const int DefaultPageSize = 10;
+        
+        /// <summary>
+        /// Список серверов.
+        /// </summary>
+        public List<GameServer> Servers { get; private set; }
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
         public readonly string? UserId;
 
-        public ListModel(GameServerService service, UserService userService)
-		{
-			_service = service;
-			Servers = _service.Servers.ToList();
-            UserId = userService.UserId;
-		}
+        /// <summary>
+        /// Номер текущей страницы.
+        /// </summary>
+        public int PageIndex { get; private set; }
 
-		public void OnGet()
-		{
-		}
-	}
+        /// <summary>
+        /// Количество страниц.
+        /// </summary>
+        public readonly int PageCount;
+
+        public ListModel(GameServerService service, UserService userService)
+        {
+            Servers = service.Servers
+                      .Where(list => list.State != GameServer.Status.Deleting)
+                      .ToList();
+            UserId = userService.UserId;
+            PageCount = (int)Math.Ceiling((decimal)Servers.Count / DefaultPageSize);
+        }
+
+        public IActionResult OnGet(int PageIndex = 1)
+        {
+            if (PageIndex <= 0)
+            {
+                return NotFound();
+            }
+
+            return SetPageList(PageIndex);
+        }
+
+        /// <summary>
+        /// Разбивает список на страницы.
+        /// </summary>
+        /// <param name="PageIndex">Номер текущей страницы.</param>
+        private IActionResult SetPageList(int PageIndex)
+        {
+            if(PageIndex > PageCount)
+            {
+                return NotFound();
+            }
+
+            this.PageIndex = PageIndex;
+
+            if ((PageIndex - 1) != 0)
+            {
+                Servers = Servers.Skip((PageIndex - 1) * DefaultPageSize).ToList();
+            }
+
+            Servers = Servers.Take(DefaultPageSize).ToList();
+            return Page();
+        }
+
+    }
 }
