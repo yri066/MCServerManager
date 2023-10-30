3f1b297f2fa99517f969bb6437fc9cd7799e18f0
yri066 <yri066.ru@gmail.com>
2023-10-30 15:38:24 +0300

    Добавлено toast уведомление при возникновении ошибки.

--- MCServerManager/wwwroot/js/Server/ControlGameServer.js
+++ MCServerManager/wwwroot/js/Server/ControlGameServer.js
@@ -74,8 +74,16 @@ function queryHandling(url, resolve) {
 			}
 
 			status = error;
-
-			$("#StatusServer").html(`Состояние сервера: ${error}`)
+			
+			$(".toast-container-extra").html(`
+			<div class="toast align-items-center text-white bg-danger w-100 p-3 show" role="alert" aria-live="assertive" aria-atomic="true">
+				<div class="d-flex">
+					<div class="toast-body text-break">
+						${error}
+					</div>
+				<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+				</div>
+			</div>`);
 		});
 }
 
