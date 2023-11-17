844b883a9a07d2764072ff47f603c99a959e0507
yri066 <yri066.ru@gmail.com>
2023-11-17 20:19:18 +0300

    Интерфейс вынесен в отдельный файл.

---
+++ MCServerManager.Library/Data/Interface/IUsersListServer.cs
@@ -0,0 +1,23 @@
+﻿namespace MCServerManager.Library.Data.Interface
+{
+    /// <summary>
+	/// Список пользователей
+	/// </summary>
+	public interface IUsersListServer<T>
+    {
+        /// <summary>
+        /// Версия списка
+        /// </summary>
+        public Guid Version { get; }
+
+        /// <summary>
+        /// Список пользователей
+        /// </summary>
+        public IEnumerable<T> UserList { get; }
+
+        /// <summary>
+        /// Количество пользователей
+        /// </summary>
+        public int Count { get; }
+    }
+}
