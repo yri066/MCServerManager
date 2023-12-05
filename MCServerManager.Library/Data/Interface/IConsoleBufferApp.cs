7432730cd50112c32e0dd22ac44ec9e7253bac30
yri066 <yri066.ru@gmail.com>
2023-12-05 19:22:34 +0300

    Интерфейс вынесен в отдельный файл.

---
+++ MCServerManager.Library/Data/Interface/IConsoleBufferApp.cs
@@ -0,0 +1,24 @@
+﻿namespace MCServerManager.Library.Data.Interface
+{
+    /// <summary>
+    /// Буфер вывода консольного приложения.
+    /// </summary>
+    public interface IConsoleBufferApp
+    {
+        /// <summary>
+        /// Версия буфера.
+        /// </summary>
+        public Guid Version { get; }
+        /// <summary>
+        /// Буфер вывода консольного приложения.
+        /// </summary>
+        public IEnumerable<string> ConsoleBuffer { get; }
+
+        /// <summary>
+        /// Получить новые сообщения из буфера.
+        /// </summary>
+        /// <param name="version">Версия буфера.</param>
+        /// <returns>Новые сообщения из буфера консольного приложения.</returns>
+        public IEnumerable<string> GetConsoleBuffer(Guid version);
+    }
+}
